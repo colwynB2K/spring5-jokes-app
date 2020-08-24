961ee1026050a20d4f3b78f959028edec2d679be
Ivo Eersels <ivo.eersels@idewe.be>
2020-08-24 16:49:20 +0200

    - Add JokeService interface and ChuckNorrisJokeService implementation - Add JokeController and a method for showing a joke mapped to the context root - Add Thymeleaf template as View

---
+++ src/main/java/guru/springframework/joke/controller/JokeController.java
@@ -0,0 +1,26 @@
+package guru.springframework.joke.controller;
+
+import guru.springframework.joke.service.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping({"/", ""})
+    String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chuck-norris";
+    }
+}
