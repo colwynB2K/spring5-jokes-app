7371f1544b9a3a350dfd91dab75954c6104867f1
Ivo Eersels <ivo.eersels@idewe.be>
2020-08-25 13:15:39 +0200

    - Add Java Spring Configuration - Using auto-wiring to inject the ChuckkNorrisQuotes class into the ChuckNorrisJokeService
    	* Add a new package 'config'
    	* Add class <AppName>Config and annotate it with @Configuration
    	* Add a method with return type of bean you want and name of bean id you want. Make sure to annotate the method with @Bean
    
    	@Configuration
    	public class JokeConfig {
    
    		@Bean
    		ChuckNorrisQuotes chuckNorrisQuotes() {
    			return new ChuckNorrisQuotes();
    		}
    	}
    
    	PLEASE NOTE: This is the way to introduce beans from external JARs into your Spring application

--- src/main/java/guru/springframework/joke/service/ChuckNorrisJokeService.java
+++ src/main/java/guru/springframework/joke/service/ChuckNorrisJokeService.java
@@ -1,6 +1,7 @@
 package guru.springframework.joke.service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,8 +9,9 @@ public class ChuckNorrisJokeService implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public ChuckNorrisJokeService() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    @Autowired
+    public ChuckNorrisJokeService(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
